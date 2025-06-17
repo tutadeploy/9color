c86bc2e311f6ddcd4251229c0f86d122c01f4dfc
josiah <tutadeploy@tutamail.com>
2025-06-17 21:30:53 +0800

    feat:update

---
+++ config/database copy.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // 数据库调试模式
+    'debug'       => true,
+    // 数据库类型
+    'type'        => 'mysql',
+    // 服务器地址
+    'hostname'    => '192.168.50.197',
+    // 数据库名
+    'database'    => '6ui',
+    // 用户名
+    'username'    => 'root',
+    // 密码
+    'password'    => 'root123456',
+    // 编码
+    'charset'     => 'utf8mb4',
+    // 端口
+    'hostport'    => '3306',
+    // 主从
+    'deploy'      => 0,
+    // 分离
+    'rw_separate' => false,
+];
